d8e5b78333ae27dc4b6d65b0d2fa951f664f87eb
bert_m <bertram.jeremy.mueller@gmail.com>
2018-05-27 14:53:10 +0100

    Working template

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		forever: {
+			server: {
+				options: {
+					index: 'app.js',
+					logDir: 'logs'
+				}
+			}
+		},
+		stylus: {
+			compile: {
+				files: {
+					'public/css/app.css': [
+						'stylus/main.styl'
+					]
+				}
+			}
+		},
+		watch: {
+			grunt: { files: ['Gruntfile.js'] },
+			stylus: {
+				files: 'stylus/*.styl',
+				tasks: ['stylus']
+			}
+		},
+		nodemon: {
+			dev: {
+				script: 'app.js'
+			}
+		},
+		concurrent: {
+			dev: {
+				tasks: ['nodemon', 'watch'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		}
+	});
+
+	// Load the plugin
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-forever');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+	// Default task(s).
+	grunt.registerTask('default', ['stylus', 'concurrent:dev']);
+
+};
---
+++ app.js
@@ -0,0 +1,25 @@
+// Dependencies
+var express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  morgan = require('morgan'),
+  serveStatic = require('serve-static'),
+  router = require('./router')
+
+var app = module.exports = express()
+
+// Configuration
+app.set('port', process.env.PORT || 8080)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+app.use(morgan('tiny'))
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')))
+app.use(serveStatic(path.join(__dirname, 'public')))
+
+// Routing
+app.use('/', router)
+
+// Start Server
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'))
+})
